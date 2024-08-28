b83efd2f8730fdc3410b5652f8106b0b1669c195
nighthawk115 <williancesardepetriz@gmail.com>
2024-08-28 19:21:08 -0300

    hp function

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -7,6 +7,7 @@ extends Node
 @export var bg : ParallaxBackground
 @export var hud : CanvasLayer
 @export var cam : Camera2D
+@export var obst_spawner : Node2D
 
 #constants
 const PLAYER_START_POS := Vector2(125, 80)
@@ -26,6 +27,9 @@ var camera_x : float
 var ground_height : int
 var last_obst
 var obst_name : String
+var spawn_distance: float
+var last_obst_position := 0.0
+var obst_spawner_pos : Vector2
 
 #arrays
 var ground_pieces = []
@@ -36,44 +40,51 @@ var obstacle_path : Array[String]
 func _ready():
 	timer.start()
 	
-	obstacle_path = ["res://scenes/acai/3.tscn", "res://scenes/acai/2.tscn", "res://scenes/acai/1.tscn"]
-	
+	obst_spawner.position = Vector2i(0, 0)
 	ground_width = ground_array[0].collision.properties.size.x
-	ground_height = ground_array[0].ground_texture.texture.get_height()
+	ground_height = ground_array[0].collision.position.y
 	screen_size = get_window().size
 	ground_pieces = ground_array
+	
+	obst_spawner.set_path(["res://scenes/acai/3.tscn", "res://scenes/acai/2.tscn", "res://scenes/acai/1.tscn"])
 	
 	order_by_position()
 	new_game()
 
 func new_game():
 	player.position = PLAYER_START_POS
+	obst_spawner.position = Vector2i(screen_size.x +20, ground_height)
 	player.velocity = Vector2(0, 0)
 	player.set_physics_process(false)
 	bg.sunshine.set_process(false)
+	game_running = false
+	speed = 0
 	
 func _process(delta):
 	if game_running:
-		player.set_physics_process(true)
-		
-		generate_obstacles()
-		
-		camera_x = cam.position.x
-		speed = START_SPEED * delta
-		player.position.x += speed
-		cam.position.x += speed
-		score += speed
-		
-		
-		show_score()
-		
-		if camera_x - ground_pieces[1].position.x > ground_width:
-			ground_pieces[0].position.x = ground_pieces[2].position.x + ground_width
-		
-		order_by_position()
-		
+		if player.is_player_dead():
+			game_running = false
+			if Input.is_action_pressed("ui_accept"):
+				get_tree().reload_current_scene()
+		else:
+			player.set_physics_process(true)
+			
+			camera_x = cam.position.x
+			speed = START_SPEED * delta
+			player.position.x += speed
+			cam.position.x += speed
+			obst_spawner.position.x += speed
+			score += speed
+			
+			show_score()
+			
+			if camera_x - ground_pieces[1].position.x > ground_width:
+				ground_pieces[0].position.x = ground_pieces[2].position.x + ground_width
+			
+			order_by_position()
+					   
 	else:
-		if Input.is_action_pressed("ui_accept"):
+		if Input.is_action_just_pressed("ui_accept"):
 			bg.sunshine.set_process(true)
 			game_running = true
 
@@ -81,43 +92,6 @@ func show_score():
 	hud.score = score / SCORE_MODIFIER
 	hud.started = true
 	#$hud.high_score_label.text = score
-	
-	
-func generate_obstacles():
-	#generate ground obstacles
-	if obstacles.is_empty() or last_obst.position.x < score + randi_range(0,50):
-		var max_obst = 3
-		var obst
-		
-		for i in range(randi() %  max_obst + 1):
-			var obstacle_object = load(obstacle_path[randi() % obstacle_path.size()])
-			var last_obst_height : float
-			
-			obst = obstacle_object.instantiate()
-			
-			var obst_size = obst.get_node("Sprite2D").texture.get_size()
-			var obst_scale = obst.get_node("Sprite2D").scale
-			var obst_x : int = screen_size.x + score + 200
-			var obst_y : int = screen_size.y - ground_height - (obst_size.x * obst_scale.y /2) +25
-			
-			print("size: ", obst_size)
-			print("scale: ", obst_scale)
-			if i == 0:
-				obst_x += i * 500
-			else:
-				obst_x += last_obst_height + (i * 30)
-			
-			last_obst = obst
-			
-			last_obst_height = obst_size.x
-			
-			add_obs(obst, obst_x, obst_y)
-		
-
-func add_obs(obst, x, y):
-	obst.position = Vector2i(x, y)
-	add_child(obst)
-	obstacles.append(obst)
 
 func order_by_position():
 	var n = ground_pieces.size()
@@ -129,5 +103,6 @@ func order_by_position():
 				ground_pieces[j + 1] = temp
 
 func _on_timer_timeout():
-	#print(obst_name)
-	print(get_window().size)
+	#print("ssize: ",screen_size)
+	#print("spawner.position: ",obst_spawner.position)
+	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,17 +2,88 @@ extends CharacterBody2D
 
 const GRAVITY : int = 1500
 const JUMP_SPEED : int = -450
+const INITIAL_HP : int =  100
+const INITIAL_STATE : int = 3
+const STATE_HP : int = 25
+const MAX_HP : int = 125
+const states = [
+	{"const_hp": 0, "const_state": 0, "const_max_hp": false},
+	{"const_hp": 25, "const_state": 1, "const_max_hp": false},
+	{"const_hp": 50, "const_state": 2, "const_max_hp": false},
+	{"const_hp": 75, "const_state": 3, "const_max_hp": false},
+	{"const_hp": 100, "const_state": 4, "const_max_hp": false},
+	{"const_hp": MAX_HP, "const_state": 4, "const_max_hp": true}
+]
+
+@export var hp : float
+
+#reiniciar o game (quando morre)
+#trocar o estado do sprite
+
+#var hp : float
+var state : int
+var previous_state := -320000
+var freeze_input_jump := false
+var is_dead := false
 
 @onready var texture := $Animation as AnimatedSprite2D
 
+func _ready():
+	hp = INITIAL_HP
+	state = INITIAL_STATE
+
 func _physics_process(delta):
-	velocity.y += GRAVITY * delta
-	
-	if is_on_floor():
-		texture.play("run")
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y = JUMP_SPEED
+	if is_dead:
+		hp = 0
+		texture.play("hurt")
 	else:
-		texture.play("jump")
+		hp -= delta
+	velocity.y += GRAVITY * delta
+	#freeze_input_jump = Input.is_action_just_released("ui_accept") and freeze_input_jump
+	
+	if Input.is_action_just_pressed("ui_down"):
+		hp -= 10
+	if Input.is_action_just_pressed("ui_up"):
+		hp += 10
+	get_all_stages()
+	
+	previous_state = state
+	if not is_dead:
+		if is_on_floor():
+			texture.play("run")
+			
+			if Input.is_action_just_pressed("ui_accept"):
+				velocity.y = JUMP_SPEED
+				#freeze_input_jump = true
+		else:
+			texture.play("jump")
 		
-	move_and_slide()
+		move_and_slide()
+	
+func get_all_stages():
+#	for item in states:
+#		if hp <= item["const_state"]:
+#			state = item["const_state"]
+#			if item["const_max_hp"]:
+#				hp = MAX_HP
+#			break
+	
+	if hp <= 0:
+		state = 0
+		is_dead = true
+	elif hp <= 25:
+		state = 1
+	elif hp <= 50:
+		state = 2
+	elif hp <= 75:
+		state = 3
+	elif hp <= 100:
+		state = 4
+	elif hp >= MAX_HP:
+		state = 4
+		hp = MAX_HP
+	
+	print("hp: ", hp, " state: ", state)
+
+func is_player_dead():
+	return is_dead
